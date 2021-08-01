22361da3c727c62cdd48ec2fecc232b59c4d7731
Radoslaw Winiecki <winiecki.radoslaw@gmail.com>
2021-08-01 14:26:45 +0200

    Create a nav menu and add CSS styles to it using the jQuery library

--- main.js
+++ main.js
@@ -59,4 +59,29 @@ $(function () {
             navMenu.css('right', '-100%');
         }
     });
+
+    $(document).scroll(() => {
+        let scrollTop = $(document).scrollTop();
+
+        if (scrollTop > 0) {
+            navMenu.addClass('is-sticky');
+            logo.css('color', '#000');
+            headerElement.css('background', '#fff');
+            navToggle.css('border-color', '#000');
+            navToggle.find('.strip').css('background-color', '#000');
+        } else {
+            navMenu.removeClass('is-sticky');
+            logo.css('color', '#fff');
+            headerElement.css('background', 'transparent');
+            navToggle.css('bordre-color', '#fff');
+            navToggle.find('.strip').css('background-color', '#fff');
+        }
+
+        headerElement.css(scrollTop >= 200 ? {
+            'padding': '0.5rem',
+            'box-shadow': '0 -4px 10px 1px #999'
+        } : {'padding': '1rem 0', 'box-shadow': 'none'});
+    });
+
+    $(document).trigger('scroll');
 });
