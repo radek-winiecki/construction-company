855656f6e0d7d23ecf7a77a16649d6fa33e75408
Radoslaw Winiecki <winiecki.radoslaw@gmail.com>
2021-08-01 14:18:33 +0200

    Create a properties slider using the jQuery library

--- main.js
+++ main.js
@@ -3,4 +3,37 @@ $(function () {
     let logo = $('#logo');
     let navMenu = $('#nav-menu');
     let navToggle = $('#nav-toggle');
+
+    $('#properties-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<a href="#" class="slick-arrow slick-prev">previous</a>',
+        nextArrow: '<a href="#" class="slick-arrow slick-next">next</a>',
+        responsive: [
+            {
+                breakpoint: 1100,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 530,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            }
+        ]
+    });
 })
