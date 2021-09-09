928c23d81188f91f55d5f2feb4b69aa87c2bd375
Radoslaw Winiecki <winiecki.radoslaw@gmail.com>
2021-09-09 03:10:09 +0200

    Create a gallery using the Masonry Grid

---
+++ app.js
@@ -0,0 +1,14 @@
+const msnry = new Macy({
+    container: '.masonry_gallery',
+    mobileFirst: true,
+    columns: 1,
+    breakAt: {
+        400: 1,
+        700: 2,
+        1100: 3,
+    },
+    margin: {
+        x: 10,
+        y: 10,
+    }
+});
